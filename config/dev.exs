eb2e82021f45d2c1d0d93b94c3aa21ed446dbf87
Thanh Huynh <huynhbathanh@gmail.com>
2022-06-17 17:40:31 +0700

    Add Tailwind library

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :rumbl, RumblWeb.Endpoint,
   secret_key_base: "ZF78S7Vo9uCp1Z+ynVvS4oYAX9v+ATBYPeOTONC2hYmI0UT9hYKliXKjAYDqWUi+",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
